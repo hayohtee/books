6204ef24f41b140ea601ad5c3445224d96454336
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-06-04 12:15:08 +0100

    Update DeleteToken()

--- internal/cache/tokens.go
+++ internal/cache/tokens.go
@@ -99,5 +99,5 @@ func (c *Cache) DeleteToken(scope, plainText string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	return c.client.HDel(ctx, fmt.Sprintf("%s:%s", scope, plainText)).Err()
+	return c.client.Del(ctx, fmt.Sprintf("%s:%s", scope, plainText)).Err()
 }
