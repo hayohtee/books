a62779313b164fa45dfe0231e6b20930a67bd410
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-06-09 14:56:06 +0100

    Update cors middleware

--- cmd/api/middlewares.go
+++ cmd/api/middlewares.go
@@ -68,6 +68,9 @@ func (app *application) cors(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Content-Type", "application/json")
+
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusNoContent)
 			return
